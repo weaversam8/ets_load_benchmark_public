0ca295ca2b2df269c96a9135f445759e36ddbc23
Sam Weaver <sweaver@relaypro.com>
2025-06-17 14:00:08 +0000

    Modify the benchmark to test the impact of write concurrency

--- ets_load_benchmark.erl
+++ ets_load_benchmark.erl
@@ -3,40 +3,48 @@
 -export([start/0]).
 
 -define(CollectTime, timer:seconds(5)).
+-define(Table1, ets_load_benchmark_public).
+-define(Table2, ets_load_benchmark_public_2).
+-define(SchedulerId(), erlang:system_info(scheduler_id)).
 
 start() ->
-    Runs = [ trunc(math:pow(2, X)) || X <- lists:seq(1, 8) ],
-    [ start(X) || X <- Runs ].
+    ets:new(?Table1, [public, named_table]),
+    ets:new(?Table2, [public, named_table, {write_concurrency, true}]),
+    Runs = [ trunc(math:pow(2, X)) || X <- lists:seq(1, 6) ],
+    io:format("Table 1 (key per worker, no write concurrency):~n"),
+    [ start(X, ?Table1) || X <- Runs ],
+    ets:delete(?Table1),
+    io:format("Table 1 (key per worker, yes write concurrency):~n"),
+    [ start(X, ?Table2) || X <- Runs ],
+    ets:delete(?Table2).
 
-start(N) ->
-    ets:new(?MODULE, [public, named_table]),
-    ets:insert(?MODULE, {c, 0}),
-    Pids = [ spawn_link(fun() -> worker_loop(idle) end) || _ <- lists:seq(1, N) ],
+start(N, TableName) ->
+    Pids = [ spawn_link(fun() -> worker_loop(idle, TableName) end) || _ <- lists:seq(1, N) ],
     M = [ erlang:monitor(process, Pid) || Pid <- Pids ],
     [ erlang:send(Pid, start) || Pid <- Pids ],
     T1 = erlang:monotonic_time(millisecond),
     timer:sleep(?CollectTime),
     [ erlang:send(Pid, stop) || Pid <- Pids],
     T2 = erlang:monotonic_time(millisecond),
-    [{c, C}] = ets:lookup(?MODULE, c),
+    Counts = ets:tab2list(TableName),
+    C = lists:foldl(fun ({{_, c}, C}, Acc) -> Acc + C end, 0, Counts),
     receive_downs(M),
-    io:format("~p => ~p /s (~p in ~p s)~n", [N, C / ((T2-T1) / 1000), C, (T2-T1)/1000]),
-    ets:delete(?MODULE).
+    io:format("~p => ~p /s (~p in ~p s)~n", [N, C / ((T2-T1) / 1000), C, (T2-T1)/1000]).
 
-worker_loop(idle) ->
+worker_loop(idle, TableName) ->
     receive
         start ->
-            worker_loop(start);
+            worker_loop(start, TableName);
         stop ->
             ok
     end;
-worker_loop(start) ->
-    ets:update_counter(?MODULE, c, [{2, 1}]),
+worker_loop(start, TableName) ->
+    ets:update_counter(TableName, {?SchedulerId(), c}, [{2, 1}], {{?SchedulerId(), c}, 0}),
     receive
         stop ->
             ok
     after 0 ->
-              worker_loop(start)
+              worker_loop(start, TableName)
     end.
 
 receive_downs([]) ->
